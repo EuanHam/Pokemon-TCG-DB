a66957ae1fb1abcd47b481100c02ba780cdc153e
EuanHam <122121529+EuanHam@users.noreply.github.com>
2023-11-30 21:14:18 -0500

    Update info.py
    Fixed a bug

--- info.py
+++ info.py
@@ -2,4 +2,8 @@
 #CHANGE BELOW (OPTIONAL)
 linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
 github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
-email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/euan-ham-b980a0249/"
+my_github_url = "https://github.com/EuanHam"
