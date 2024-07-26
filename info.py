fe14da3108a8aae800cd71554979aea43fd16e96
Euan Ham <122121529+EuanHam@users.noreply.github.com>
2024-07-26 00:34:50 -0400

    Update info.py

--- info.py
+++ info.py
@@ -1,9 +1,7 @@
-
-#CHANGE BELOW (OPTIONAL)
 linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
 github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
 
-
-#CHANGE BELOW
 my_linkedin_url = "https://www.linkedin.com/in/euan-ham-b980a0249/"
 my_github_url = "https://github.com/EuanHam"
+my_email_address = "euanham05@gmail.com"
